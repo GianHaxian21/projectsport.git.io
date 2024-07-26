3c35bb67c284d582fbb4fa9aaeb084eb636c7515
Gian Haxian <elmitch0315@gmail.com>
2024-07-26 08:48:19 +0800

    COMMIT

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // script.js
 document.getElementById('downloadButton').addEventListener('click', function() {
     // Replace 'resume.pdf' with the actual path to your resume file
-    var fileUrl = 'Elexis Falceso_Resume_CV.pdf';
+    var fileUrl = 'ELEXIS_FALCESO.pdf';
     
     var a = document.createElement('a');
     a.href = fileUrl;
-    a.download = 'Elexis Falceso_Resume_CV.pdf';
+    a.download = 'ELEXIS_FALCESO.pdf';
     document.body.appendChild(a);
     a.click();
     document.body.removeChild(a);
